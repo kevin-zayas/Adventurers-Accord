3afd3a3ad46a01cde7e07db1229cdc3e2a985798
Kevin Zayas <Zayas.kevin1999@gmail.com>
2024-02-06 16:31:44 -0500

    Added Draw card button functionality

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -14,20 +14,4 @@ public class Card : ScriptableObject
     public string cardDescription;
     public Sprite spriteImage;
 
-
-    public Card()
-    {
-
-    }
-
-    public Card(int Id, string CardName, int Cost, int PhysPower, int MagPower, string CardDescription, Sprite SpriteImage)
-    {
-        id = Id;
-        cardName = CardName;
-        cost = Cost;
-        physPower = PhysPower;
-        magPower = MagPower;
-        cardDescription = CardDescription;
-        spriteImage = SpriteImage;
-    }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,22 +4,18 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public List<Card> deck = new List<Card>();
+    public List<Card> deck;
     public Transform[] cardSlots;
     public bool[] availableCardSlots;
     public GameObject AdventurerCard;
-    public Dictionary<int, Card> dict = new();
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        dict.Add(0, new Card());
         deck = PlayerDeck.staticDeck;
-        GameObject card = Instantiate(AdventurerCard, cardSlots[0].transform.position, Quaternion.identity);
-        card.transform.SetParent(cardSlots[0].transform); //experiment with true false
-        card.GetComponent<DisplayCard>().LoadCardData(CardDatabase.cardDatabase[0]);
+        DrawCard();
 
 
 
@@ -33,9 +29,11 @@ public class GameManager : MonoBehaviour
 
     public void DrawCard()
     {
+        print(deck.Count);
         if (deck.Count >= 1)
         {
             Card randomCard = deck[Random.Range(0, deck.Count)];
+            print(randomCard);
 
             for (int i = 0; i < availableCardSlots.Length; i++)
             {
@@ -44,7 +42,10 @@ public class GameManager : MonoBehaviour
                     //populate card from deck here. May want to use instantiate instead of setActive to avoid a large number of cards in the scene.
                     //randomCard.SetActive();
                     //randomCard.tranform.position = cardSlots[i].position;
-                    
+                    GameObject card = Instantiate(AdventurerCard, cardSlots[i].transform.position, Quaternion.identity);
+                    card.transform.SetParent(cardSlots[i].transform); //experiment with true false
+                    card.GetComponent<DisplayCard>().LoadCardData(randomCard);
+
                     availableCardSlots[i] = false;
                     deck.Remove(randomCard);
 
