1731492ce679d7a8c3671cee4cc73a6d735cc539
Kevin Zayas <Zayas.kevin1999@gmail.com>
2024-04-14 01:24:15 -0400

    Added logic to factor in gold amount when determining endgame ranking
    Also changed popup title to defeat if not ranked number 1

--- Assets/Scripts/PopUp Components/GameOverPopUp.cs
+++ Assets/Scripts/PopUp Components/GameOverPopUp.cs
@@ -3,6 +3,7 @@ using FishNet.Object;
 using FishNet.Object.Synchronizing;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 
@@ -19,43 +20,29 @@ public class GameOverPopUp : NetworkBehaviour
     {
         int maxReputation = 0;
         List<Player> playerList = new(GameManager.Instance.Players);
-        playerList.Sort((a, b) => b.Reputation.CompareTo(a.Reputation));
-        maxReputation = playerList[0].Reputation;
-        print(maxReputation);
+        playerList = playerList.OrderByDescending(x => x.Reputation).ThenByDescending(x => x.Gold).ToList();
 
         List<string> rankingTextList = new();
         int prevRanking = 0;
         int prevReputation = 0;
+        int prevGold = 0;
+        maxReputation = playerList[0].Reputation;
 
-        foreach(Player player in playerList)
+        foreach (Player player in playerList)
         {
-            if (player.Reputation != prevReputation)
+            if (player.Reputation != prevReputation || player.Gold != prevGold)
             {
                 prevRanking++;
                 prevReputation = player.Reputation;
+                prevGold = player.Gold;
             }
-            rankingTextList.Add(string.Format(rankingText, prevRanking, player.PlayerID+1, player.Reputation));
+            rankingTextList.Add(string.Format(rankingText, prevRanking, player.PlayerID + 1, player.Reputation, player.Gold));
         }
 
-        foreach(Player player in playerList)
+        foreach (Player player in playerList)
         {
-            
+
             TargetInitializeGameOverPopUp(player.Owner, rankingTextList, player.Reputation == maxReputation);
-        }
-        //ObserversInitializeGameOverPopUp(rankingTextList);
-    }
-
-    [ObserversRpc]
-    public void ObserversInitializeGameOverPopUp(List<string> rankings)
-    {
-        print("initializing game over pop up");
-        transform.SetParent(GameObject.Find("Canvas").transform);
-        transform.localPosition = Vector3.zero;
-
-        for (int i = 0; i < rankings.Count; i++)
-        {
-            GuildRankings[i].gameObject.SetActive(true);
-            GuildRankings[i].text = rankings[i];
         }
     }
 
@@ -73,5 +60,6 @@ public class GameOverPopUp : NetworkBehaviour
         }
 
         if (victory) titleText.text = "Victory!";
+        else titleText.text = "Defeat!";
     }
 }
