61f675a3fd3d6ae2b44d6548873e65d6b5069c6f
Kevin Zayas <Zayas.kevin1999@gmail.com>
2024-04-19 10:19:41 -0400

    Added Server Restart Button

--- Assets/Scripts/UI Components/MultiplayerMenuView.cs
+++ Assets/Scripts/UI Components/MultiplayerMenuView.cs
@@ -1,5 +1,8 @@
 using FishNet;
+using System.Collections;
+using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class MultiplayerMenuView : View
@@ -15,6 +18,9 @@ public class MultiplayerMenuView : View
 
     [SerializeField]
     private Button creditsButton;
+
+    [SerializeField]
+    private Button restartServerButton;
 
     public override void Initialize()
     {
@@ -35,7 +41,54 @@ public class MultiplayerMenuView : View
             popUp.transform.localPosition = Vector3.zero;
         });
 
+        restartServerButton.onClick.AddListener(() =>
+        {
+            
+
+            SendPostRequest();
+        });
+
         base.Initialize();
+    }
+
+    public void SendPostRequest()
+    {
+        StartCoroutine(PostRequest());
+    }
+
+    private IEnumerator PostRequest()
+    {
+        print("Restarting Server");
+        string token = "efe81a97a8bd587f5b1172d7c025796b";
+        string matchId = "6b1a76142bbb4d3ead2733e43c81e77c-studio-us-east.playflow.dev";
+        string url = "https://api.cloud.playflow.app/restart_game_server";
+
+        // Create a new UnityWebRequest
+        UnityWebRequest request = new UnityWebRequest(url, "POST");
+
+        // Set headers
+        request.SetRequestHeader("accept", "application/json");
+        request.SetRequestHeader("match-id", matchId);
+        request.SetRequestHeader("token", token);
+
+        // Set the POST body (empty in this case)
+        byte[] bodyRaw = new byte[0];
+        request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+
+        // Send the request
+        yield return request.SendWebRequest();
+
+        // Check for errors
+        if (request.result != UnityWebRequest.Result.Success)
+        {
+            Debug.LogError("Error: " + request.error);
+        }
+        else
+        {
+            Debug.Log("POST request successful!");
+            Debug.Log("Response: " + request.downloadHandler.text);
+        }
     }
 
     public void Quit()
