51ff1801201fe51266068a70b08c259b7325f63f
Kevin Zayas <Zayas.kevin1999@gmail.com>
2024-08-23 10:13:30 -0400

    Update SpellCard.cs

--- Assets/_Scripts/Card Components/SpellCard.cs
+++ Assets/_Scripts/Card Components/SpellCard.cs
@@ -7,22 +7,23 @@ using UnityEngine.UI;
 
 public class SpellCard : Card
 {
+    #region SyncVars
     [field: SyncVar] public bool IsGreaseSpell { get; private set; }
+    #endregion
 
-    [SerializeField] private TMP_Text physicalPowerText;
+    #region UI Elements
+    [SerializeField] private Image cardImage;
+    [SerializeField] private TMP_Text descriptionText;
     [SerializeField] private TMP_Text magicalPowerText;
     [SerializeField] private TMP_Text nameText;
-    [SerializeField] private TMP_Text descriptionText;
-    [SerializeField] private Image cardImage;
+    [SerializeField] private TMP_Text physicalPowerText;
+    #endregion
 
-    private void Start()
-    {
-        //nameText.text = Name;
-        //descriptionText.text = Description;
-        //physicalPowerText.text = PhysicalPower.ToString();
-        //magicalPowerText.text = MagicalPower.ToString();
-    }
-
+    /// <summary>
+    /// Updates the card's parent transform on all clients, adjusting its scale if the parent is a Quest.
+    /// </summary>
+    /// <param name="parent">The new parent transform.</param>
+    /// <param name="worldPositionStays">Whether to maintain the world position of the card.</param>
     [ObserversRpc(BufferLast = true)]
     protected override void ObserversSetCardParent(Transform parent, bool worldPositionStays)
     {
@@ -33,15 +34,22 @@ public class SpellCard : Card
         this.transform.SetParent(parent, worldPositionStays);
     }
 
+    /// <summary>
+    /// Loads the card data and updates the relevant SyncVars on the server.
+    /// </summary>
+    /// <param name="cardData">The card data to load.</param>
     [Server]
     public override void LoadCardData(CardData cardData)
     {
         IsGreaseSpell = cardData.IsGreaseSpell;
 
         base.LoadCardData(cardData);
-        //ObserversLoadCardData(cardData);
     }
 
+    /// <summary>
+    /// Updates the card's visual representation on all clients based on the provided card data.
+    /// </summary>
+    /// <param name="cardData">The card data to load into the visual elements.</param>
     [ObserversRpc(BufferLast = true)]
     protected override void ObserversLoadCardData(CardData cardData)
     {
@@ -53,6 +61,11 @@ public class SpellCard : Card
         cardImage.sprite = CardDatabase.Instance.SpriteMap[cardData.CardName];
     }
 
+    /// <summary>
+    /// Updates the target client with the copied card data from the original card.
+    /// </summary>
+    /// <param name="connection">The network connection of the target client.</param>
+    /// <param name="originalCard">The original card to copy data from.</param>
     [TargetRpc]
     public override void TargetCopyCardData(NetworkConnection connection, Card originalCard)
     {
@@ -64,6 +77,5 @@ public class SpellCard : Card
         magicalPowerText.text = card.MagicalPower.ToString();
         nameText.text = card.CardName;
         descriptionText.text = card.CardDescription;
-
     }
 }
