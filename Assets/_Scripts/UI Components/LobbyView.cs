fff7ce2a0bc4492fe38f74664774291b68de49ba
Kevin Zayas <Zayas.kevin1999@gmail.com>
2025-04-08 11:19:15 -0700

    Added Guild Selection Functionality to the Lobby View
    Added Guild Description and placeholder Guild Icons

--- Assets/_Scripts/UI Components/LobbyView.cs
+++ Assets/_Scripts/UI Components/LobbyView.cs
@@ -10,8 +10,6 @@ public class LobbyView : View
     [SerializeField] Button howToPlayButton;
     [SerializeField] Button readyButton;
     [SerializeField] Button startButton;
-
-    [SerializeField] TMP_Text playerList;
     [SerializeField] TMP_Text readyButtonText;
 
     [SerializeField] Button fightersGuildButton;
@@ -22,15 +20,26 @@ public class LobbyView : View
     private Button selectedButton;
 
     [SerializeField] TMP_Text guildText;
-
     [SerializeField] GameObject[] playerEntries;
 
+    [SerializeField] Sprite FightersGuildSprite;
+    [SerializeField] Sprite DefaultGuildSprite;
+
+    [field: SerializeField, TextArea(3, 6)] private string fightersGuildText;
+    [field: SerializeField, TextArea(3, 6)] private string magesGuildText;
+    [field: SerializeField, TextArea(3, 6)] private string thievesGuildText;
+    [field: SerializeField, TextArea(3, 6)] private string merchantsGuildText;
+    [field: SerializeField, TextArea(3, 6)] private string assassinsGuildText;
+
+    private readonly Dictionary<GuildType, Sprite> SpriteMap = new();
+    private readonly Dictionary<GuildType, string> DescriptionMap = new();
 
     public override void Initialize()
     {
         howToPlayButton.onClick.AddListener(() => PopUpManager.Instance.CreateHowToPlayPopUp());
         readyButton.onClick.AddListener(() => Player.Instance.TogglePlayerIsReady());
         InitializeGuildButtons();
+        InitializeGuildMaps();
 
         //if (InstanceFinder.IsServer)
         if (Player.Instance.IsStartingPlayer.Value) // could just check is player 1
@@ -63,6 +72,7 @@ public class LobbyView : View
             
 
             playerEntries[i].GetComponentInChildren<TMP_Text>().text = playerText;
+            playerEntries[i].GetComponentInChildren<Image>().sprite = SpriteMap[player.GuildType];
         }
 
         readyButton.interactable = Player.Instance.GuildType != GuildType.None;
@@ -75,7 +85,7 @@ public class LobbyView : View
         if (selectedButton != null) selectedButton.interactable = true;
         guildButton.interactable = false;
         selectedButton = guildButton;
-        guildText.text = $"{guildType} Selected";
+        guildText.text = DescriptionMap[guildType];
         print(guildType);
     }
 
@@ -86,5 +96,22 @@ public class LobbyView : View
         thievesGuildButton.onClick.AddListener(() => SetGuildType(GuildType.ThievesGuild, thievesGuildButton));
         merchantsGuildButton.onClick.AddListener(() => SetGuildType(GuildType.MerchantsGuild, merchantsGuildButton));
         assassinsGuildButton.onClick.AddListener(() => SetGuildType(GuildType.AsassinsGuild, assassinsGuildButton));
+    }
+
+    private void InitializeGuildMaps()
+    {
+        SpriteMap.Add(GuildType.FightersGuild, FightersGuildSprite);
+        SpriteMap.Add(GuildType.MagesGuild, DefaultGuildSprite);
+        SpriteMap.Add(GuildType.ThievesGuild, DefaultGuildSprite);
+        SpriteMap.Add(GuildType.MerchantsGuild, DefaultGuildSprite);
+        SpriteMap.Add(GuildType.AsassinsGuild, DefaultGuildSprite);
+        SpriteMap.Add(GuildType.None, DefaultGuildSprite);
+
+        DescriptionMap.Add(GuildType.FightersGuild, fightersGuildText);
+        DescriptionMap.Add(GuildType.MagesGuild, magesGuildText);
+        DescriptionMap.Add(GuildType.ThievesGuild, thievesGuildText);
+        DescriptionMap.Add(GuildType.MerchantsGuild, merchantsGuildText);
+        DescriptionMap.Add(GuildType.AsassinsGuild, assassinsGuildText);
+        DescriptionMap.Add(GuildType.None, "No Guild Selected");
     }
 }
