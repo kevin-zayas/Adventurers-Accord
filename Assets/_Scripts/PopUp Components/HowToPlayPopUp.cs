2fef1a527f1731016c013895a17656d3f60db18c
Kevin Zayas <Zayas.kevin1999@gmail.com>
2025-04-24 11:50:58 -0700

    Added Controls Screen and added functional Directory to How To Play Pop Up

--- Assets/_Scripts/PopUp Components/HowToPlayPopUp.cs
+++ Assets/_Scripts/PopUp Components/HowToPlayPopUp.cs
@@ -4,14 +4,13 @@ using UnityEngine.UI;
 
 public class HowToPlayPopUp : PopUp
 {
-    [SerializeField]
-    private List<GameObject> pages = new();
+    [SerializeField] private List<GameObject> pages = new();
+    [SerializeField] private List<Button> pageButtons = new();
 
-    [SerializeField]
-    private int pageIndex = 0;
 
-    [SerializeField]
-    private GameObject currentPage;
+    [SerializeField] private int pageIndex = 0;
+
+    [SerializeField] private GameObject currentPage;
 
     [SerializeField] private GameObject content;
 
@@ -29,32 +28,52 @@ public class HowToPlayPopUp : PopUp
 
         leftButton.onClick.AddListener(() =>
         {
-            ChangePage(-1);
+            ChangeToPage(pageIndex - 1);
         });
 
         rightButton.onClick.AddListener(() =>
         {
-            ChangePage(1);
+            ChangeToPage(pageIndex + 1);
         });
 
         currentPage = Instantiate(pages[pageIndex], Vector2.zero, Quaternion.identity);
         currentPage.transform.SetParent(content.transform, false);
 
         leftButton.interactable = false;
+        InitiaizePageButtons();
     }
 
-    private void ChangePage(int changeIndex)
+    private void Update()
     {
-        Destroy(currentPage);
-        pageIndex = (pageIndex + changeIndex) % pages.Count;
+        pageButtons[pageIndex].Select();
+    }
 
-        currentPage = Instantiate(pages[pageIndex], Vector2.zero, Quaternion.identity);
+    private void InitiaizePageButtons()
+    {
+        foreach (Button button in pageButtons)
+        {
+            button.onClick.AddListener(() =>
+            {
+                int index = pageButtons.IndexOf(button);
+                ChangeToPage(index);
+            });
+        }
+    }
+
+    private void ChangeToPage(int newPageIndex)
+    {
+        if (this.pageIndex == newPageIndex) return;
+
+        Destroy(currentPage);
+        this.pageIndex = newPageIndex;
+
+        currentPage = Instantiate(pages[newPageIndex], Vector2.zero, Quaternion.identity);
         currentPage.transform.SetParent(content.transform, false);
 
-        if (pageIndex == 0) leftButton.interactable = false;
+        if (this.pageIndex == 0) leftButton.interactable = false;
         else leftButton.interactable = true;
 
-        if (pageIndex == pages.Count -1) rightButton.interactable = false;
+        if (this.pageIndex == pages.Count -1) rightButton.interactable = false;
         else rightButton.interactable = true;
     }
 }
