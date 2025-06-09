01af4ddbff1aec57111644774b40b2462329d83f
Kevin Zayas <Zayas.kevin1999@gmail.com>
2025-06-09 15:40:12 -0700

    Added basic example card animation functionality
    moved some scripts to a Utility Folder

---
+++ Assets/_Scripts/VisualCardsHandler.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class VisualCardsHandler : MonoBehaviour
+{
+
+    public static VisualCardsHandler instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
